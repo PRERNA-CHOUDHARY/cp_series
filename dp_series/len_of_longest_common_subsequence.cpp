a54f8b968f514395151c3e0d82b57c3d986b899d
Prerna Choudhary <choudharyprerna493@gmail.com>
2022-11-22 19:24:41 +0530

    Create len_of_longest_common_subsequence.cpp

---
+++ dp_series/len_of_longest_common_subsequence.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ans(string s1,string s2,int i,int j,vector<vector<int>> &dp){
+    if (i<0 || j<0) return 0; 
+    if(dp[i][j]!=-1) return dp[i][j];
+    int match=-1e9,not_match =-1e9;
+    if(s1[i]==s2[j])
+       match=ans(s1,s2,i-1,j-1,dp)+1;
+      if(s1[i]!=s2[j])  not_match= max(ans(s1,s2,i-1,j,dp)+0,ans(s1,s2,i,j-1,dp)+0);
+    return dp[i][j]=max(not_match,match);
+}
+
+int main()
+{
+    string s1="prerna",s2="prnrea",res="";
+    vector<vector<int>> dp(s1.length()+1,vector<int>(s2.length()+1,-1));
+    cout<<ans(s1,s2,s1.length()-1,s2.length()-1,dp);
+
+}

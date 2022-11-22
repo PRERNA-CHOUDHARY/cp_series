b9831106c86ea687daa9a3f8e61cb0c709c733e7
Prerna Choudhary <choudharyprerna493@gmail.com>
2022-11-22 19:24:30 +0530

    Create 0_1_knapsack.cpp

---
+++ dp_series/0_1_knapsack.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int knapsack(vector<int> val,vector<int> wt,int i,int w, vector<vector<int>>  &dp)
+{
+    if(w<0 || i<0) return 0;
+    if(dp[i][w]!=-1 ) return dp[i][w];
+    int not_pick=knapsack(val,wt,i-1,w,dp)+0;
+    int pick=-1e9;
+    if(wt[i]<=w) pick=knapsack(val,wt,i-1,w-wt[i],dp)+val[i];
+    return dp[i][w]=max(pick,not_pick);
+}
+int main()
+{
+  vector<int> val={30,40,50},wt={3,2,4};
+ int w=5;
+    int n=val.size();
+    vector<vector<int>> dp(n,vector<int>(w+1,-1));
+    cout<<knapsack(val,wt,n-1,w,dp);
+    return 0;
+}

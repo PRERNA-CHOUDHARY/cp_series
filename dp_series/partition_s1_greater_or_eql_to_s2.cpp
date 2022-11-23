4c8b73f1739d1fed7d750b4a19a926810d163947
Prerna Choudhary <choudharyprerna493@gmail.com>
2022-11-23 22:33:16 +0530

    Update partition_s1_greater_or_eql_to_s2.cpp

--- dp_series/partition_s1_greater_or_eql_to_s2.cpp
+++ dp_series/partition_s1_greater_or_eql_to_s2.cpp
@@ -1,32 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-/*Partition a set into two subsets such 
+/*Partition a set into two subsets such
 that the difference of subset sums is minimum.*/
-int target_sum(vector<int>v,int i,int target,vector<vector<int>>&dp)
+int target_sum(vector<int> v, int i, int target, vector<vector<int>> &dp)
 {
-    if(0==target )
-     return 1;
-     if(i==0) return v[0]==target;
-     if(dp[i][target]!=-1) return dp[i][target];
-    if(target<0 || i <0) return 0;
-    return dp[i][target]= (target_sum(v,i-1,target-v[i],dp) +target_sum(v,i-1,target,dp));
+    if (i == 0)
+    {
+        if (target == 0 && v[0] == 0)
+            return 2;
+        if (target == 0 || target == v[0])
+            return 1;
+        return 0;
+    }
+    
+    if (dp[i][target] != -1)
+        return dp[i][target];
+
+    int nottake = target_sum(v, i - 1, target, dp);
+    int take = 0;
+    if (v[i] <= target)
+        take = target_sum(v, i - 1, target - v[i], dp);
+    return dp[i][target] = take + nottake;
 }
 int main()
 {
-    
-    vector<int> v{1,2,3};
-    int n=v.size();
-    // int target=1;
-    int d=1;
-    int sum=1;
-    for(auto i:v)
-    sum+=i;
 
-    vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+    vector<int> v{1, 2, 4};
+    int n = v.size();
+    // int target=1;
+    int d = 1;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += v[i];
+    // cout<<sum;
+    vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
     if(sum-d<0 || (sum-d)%2) cout<<0;
     else
-    cout<<target_sum(v,n-1,(sum-d)/2,dp);
+        cout << target_sum(v, n - 1, (sum - d) / 2, dp);
     return 0;
-    
-
 }

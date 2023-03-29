f9a99b6e66cb6f21865a6e4bc334aa92ab07b079
Prerna Choudhary <choudharyprerna493@gmail.com>
2023-03-29 20:49:43 +0530

    Create trap_rainWater.cpp

---
+++ array/trap_rainWater.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        // code here
+        long long sum=0;
+        int left_height[n]={0},right_height[n]={0};
+        left_height[0]=arr[0];
+        right_height[n-1]=arr[n-1];
+        for(int i=1;i<n;i++)
+        {
+            left_height[i]=max(arr[i],left_height[i-1]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            right_height[i]=max(arr[i],right_height[i+1]);
+        }
+        for(int i=1;i<n-1;i++)
+        {
+            if(arr[i]<left_height[i-1] && arr[i]<right_height[i+1])
+            {
+                
+                sum+=min(right_height[i+1],left_height[i-1])-arr[i];
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
